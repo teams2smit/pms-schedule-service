825b53044ac289666ed1b58df13591d844f893c3
Jay Jadhav <jadhav.jay0253@gmail.com>
2022-02-24 22:35:36 +0530

    All done dont touch the code

--- src/main/java/com/pms/schedule/MedicalRepScheduleApplication.java
+++ src/main/java/com/pms/schedule/MedicalRepScheduleApplication.java
@@ -1,8 +1,11 @@
 package com.pms.schedule;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -11,6 +14,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 
 @SpringBootApplication
@@ -44,6 +49,22 @@ public class MedicalRepScheduleApplication {
                 .title("Schedule Service")
                 .version("v2.0")
                 .build();
+    }
+
+    @Bean
+    @Autowired
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+        String ip = null;
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        config.setNonSecurePort(8088);
+        config.setIpAddress(ip);
+        config.setPreferIpAddress(true);
+        return config;
     }
 
 }
